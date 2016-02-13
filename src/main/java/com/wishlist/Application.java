434bd0dd9b08c3bd06a3b3760538284c34c88dba
odahovskiy <odahovskiy@gmail.com>
2016-02-13 10:53:46 +0200

    add configuration for remote db work

--- src/main/java/com/wishlist/Application.java
+++ src/main/java/com/wishlist/Application.java
@@ -1,8 +1,5 @@
 package com.wishlist;
 
-import com.wishlist.model.User;
-import com.wishlist.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,21 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository repository;
-
     @Override
     public void run(String... args) throws Exception {
-        this.repository.deleteAll();
-
-        // save a couple of customers
-        this.repository.save(new User("Alice", "Smith"));
-        this.repository.save(new User("Bob", "Smith"));
-        this.repository.save(new User("Vova", "Gluk", true));
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        this.repository.findAllOnlyOnline().forEach(System.out::println);
 
     }
 
