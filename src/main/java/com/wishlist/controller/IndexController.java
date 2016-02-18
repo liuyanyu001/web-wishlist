857f83b5101d9ee52ecb96fc709a0a9dda7041aa
odahovskiy <odahovskiy@gmail.com>
2016-02-18 09:44:36 +0200

    Change routes for login and profile page

--- src/main/java/com/wishlist/controller/IndexController.java
+++ src/main/java/com/wishlist/controller/IndexController.java
@@ -1,14 +1,8 @@
 package com.wishlist.controller;
 
-import com.wishlist.model.User;
 import com.wishlist.util.auth.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -27,14 +21,10 @@ public class IndexController {
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
         if (securityUtil.isCurrentUserAuth()) {
-            return "redirect:/me/";
+            return "redirect:/profile/me/";
         }
         return "index";
     }
 
-    @RequestMapping(value = "me", method = RequestMethod.GET)
-    public String me(@ModelAttribute(value = "userBean") User user, ModelMap map) {
-        map.addAttribute("user", user);
-        return "me";
-    }
+
 }
---
+++ src/main/java/com/wishlist/controller/ProfileController.java
@@ -0,0 +1,23 @@
+package com.wishlist.controller;
+
+
+import com.wishlist.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@RequestMapping("/profile")
+@SessionAttributes("userBean")
+public class ProfileController {
+
+    @RequestMapping(value = "me", method = RequestMethod.GET)
+    public String me(@ModelAttribute(value = "userBean") User user, ModelMap map) {
+        map.addAttribute("user", user);
+        return "me";
+    }
+
+}
