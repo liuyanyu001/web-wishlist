276bb1d54059dbe9460b5f73416c920990974747
odahovskiy <odahovskiy@gmail.com>
2016-02-18 09:35:29 +0200

    Add application root context

--- src/main/java/com/wishlist/Application.java
+++ src/main/java/com/wishlist/Application.java
@@ -1,15 +1,25 @@
 package com.wishlist;
 
 import com.wishlist.service.IUserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @SpringBootApplication 
-public class Application  implements CommandLineRunner {
+public class Application  extends SpringBootServletInitializer implements CommandLineRunner  {
 
-    @Autowired private IUserService userService;
+    protected final Log logger = LogFactory.getLog(getClass());
 
     @Override
     public void run(String... args) throws Exception {
@@ -17,5 +27,24 @@ public class Application  implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext rootAppContext = createRootApplicationContext(servletContext);
+
+        if (rootAppContext != null) {
+            ServletRegistration.Dynamic dispatcher =
+                    servletContext.addServlet("dispatcher", new DispatcherServlet(rootAppContext));
+
+            dispatcher.addMapping("/");
+            dispatcher.setLoadOnStartup(1);
+        }
+        else {
+            this.logger.debug("No ContextLoaderListener registered, as "
+                    + "createRootApplicationContext() did not "
+                    + "return an application context");
+        }
     }
 }
