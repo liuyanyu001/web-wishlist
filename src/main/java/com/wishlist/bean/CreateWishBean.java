cf6fee913875190c506560cce426bf5a987f1a5d
odahovskiy <odahovskiy@gmail.com>
2016-02-18 20:19:32 +0200

    Add create Wish bean

---
+++ src/main/java/com/wishlist/bean/CreateWishBean.java
@@ -0,0 +1,52 @@
+package com.wishlist.bean;
+
+
+public class CreateWishBean {
+
+    private String title;
+    private String picture;
+    private String description;
+    private String ownerId;
+
+    public CreateWishBean() {
+    }
+
+    public CreateWishBean(String title, String picture, String description, String ownerId) {
+        this.title = title;
+        this.picture = picture;
+        this.description = description;
+        this.ownerId = ownerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+}
